758da2e85757f8652dceb89755f248da1f8ee520
dsapanaro <diana.sapanaro@gmail.com>
2014-10-23 17:22:42 -0400

    Printed Male Artists

--- SAD_gender.py
+++ SAD_gender.py
@@ -4,4 +4,10 @@ with open("colon3_table_export.csv", "r") as f:
 
 	reader = csv.reader(f)
 	for row in reader:
-		print (row[26])
+		#print (row [1] + row[26])
+		#if row[26] == "y":
+			#print(row[1])
+		if row[26] == '':
+			print("This is a male artist", row[1])
+
+
