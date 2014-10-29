f536c4568f4671e4bd459463b1b6b0fb758e2706
dsapanaro <diana.sapanaro@gmail.com>
2014-10-28 20:31:02 -0400

    Starting a blank list for female artists

--- SAD_gender.py
+++ SAD_gender.py
@@ -2,12 +2,13 @@ import csv
 
 with open("colon3_table_export.csv", "r") as f:
 
+	Female_Artists = []
+
 	reader = csv.reader(f)
 	for row in reader:
 		#print (row [1] + row[26])
-		#if row[26] == "y":
 			#print(row[1])
-		if row[26] == '':
-			print("This is a male artist:", row[1])
+		if row[26] == "y":
+		
 
 
