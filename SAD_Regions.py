dc188f5ab9ce41f2c2462cf112102eaef0baa307
dsapanaro <diana.sapanaro@gmail.com>
2014-11-06 17:29:12 -0500

    Starting to create regions of Spain CSV

---
+++ SAD_Regions.py
@@ -0,0 +1,14 @@
+import csv
+
+with open("colon3_table_export.csv", "r") as f:
+
+	Spanish_Regions = []
+
+	reader = csv.reader(f)
+	for row in reader:
+		Spanish_Regions.append([row[1],row[22]])
+
+with open('Regions_Spain.csv', 'w') as csvfile:	
+	writer = csv.writer(csvfile)
+    for a_row in Spanish_Regions:
+    	writer.writerow(a_row)
