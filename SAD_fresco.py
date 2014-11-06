6fdc54e2fa6739b9f198aa7f9cdf639489fcbf52
aprovo <aprovo@gmail.com>
2014-11-06 18:01:22 -0500

    tried fuzzywuzzy module and fixed for loop

--- SAD_fresco.py
+++ SAD_fresco.py
@@ -7,25 +7,40 @@
 #have two lists of different lengths, want to compare (intersect?) the two and determine which are probable matches.
 
 
-import csv, nltk, difflib, fuzzywuzzy
-
+import csv, difflib
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from fuzzywuzzy import utils
+from fuzzywuzzy.string_processing import StringProcessor
+#import nltk
 SADnames = []
 FRESCOnames = []
+HighMatches = {}
 
-def compare(self,SADstring,FRESCOstring):
+def compare(SADstring,FRESCOstring):
 #going to pass the SADstring to the function that will then perform levenstein matching/fuzzy match and 
 # if the percent match is over 75%, dump the FRESCO name into a new column in the CSV.
 #to figure out: how to make a new column in an already existing CSV (diana is working on this)	
 #want to break up each name string into words, then see if substrings are present, and what percentage of the words are the same.
 
-    return difflib.SequenceMatcher(x=SADstring.lower(), y=FRESCOstring.lower()).ratio() > 0.9
-    #return fuzz.partial_ratio(SADstring, FRESCOstring)
+    #return difflib.SequenceMatcher(x=SADstring.lower(), y=FRESCOstring.lower()).ratio() > 0.9
+  #  comparison = difflib.SequenceMatcher(None, SADstring, FRESCOstring)
+  #  comparison.ratio()
+  #  if comparison.ratio() > .5:
+   # 	print(SADstring, ":",FRESCOstring)
+    c = fuzz.token_set_ratio(SADstring, FRESCOstring)
+    if c > 90:
+    	print("high match:", SADstring, ":", FRESCOstring, c)
+   
+   # if c < 90 and c > 75:
+    #	print ("mid match:", SADstring, ":", FRESCOstring, c)
+
 with open("colon3_table_export.csv", "r") as f:
 
 	reader = csv.reader(f)
 	for row in reader:
 		mainSADname = row[1]
-		#SADnames.append(mainSADname)
+		SADnames.append(mainSADname)
 	#print (SADnames)
 with open("FRESCO_spanish_names.csv", "r", encoding="ISO-8859-1") as f:
 
@@ -34,12 +49,17 @@ with open("FRESCO_spanish_names.csv", "r", encoding="ISO-8859-1") as f:
 		frescoSH = row[0]
 		FRESCOnames.append(frescoSH)
 	#print(FRESCOnames)
-#for x in SADnames:
-	#print(x)
-#for y in FRESCOnames:
-#	y = FRESCOname
+
+for x in SADnames:
+	SADname = x
+
+	for y in FRESCOnames:
+
+		FRESCOname = y
+
+		compare(SADname, FRESCOname)
+	#print(compare)
 
 
-compare(mainSADname, FRESCOnames)
 #' '.join([j for j, m in zip(SADnames.split(), FRESCOnames.split()) if j==m])
 #eventually need to write back into this CSV, and tell it exactly where to write the new column
