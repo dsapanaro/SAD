f2e1a1b0fc2f6efedd430cc74868e1d4b677452c
dsapanaro <diana.sapanaro@gmail.com>
2014-11-06 10:53:51 -0500

    ATTEMPTING better formatting

--- SAD_gender.py
+++ SAD_gender.py
@@ -8,14 +8,17 @@ with open("colon3_table_export.csv", "r") as f:
 	for row in reader:
 		#print (row[1] + row[26])
 			#print(row[1])
-		if row[26] == "y":
+		if row[26] == "y" or "Y":
 			Female_Artists.append(row[1])
-	print(Female_Artists)
+	#print(Female_Artists)
 
-with open('Female_Artists.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=' ', quotechar=' ')
+with open('Female_Artists.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile.excel class='excel_tab')
     writer.writerows(Female_Artists)
 
 
 
 #problems: space between every letter, separating into two columns, want 'Vives, Carmen' to be in one column
+#excel_tab and excel might be possible solutions 
+#dialect='excel_tab'
+#class='excel'
