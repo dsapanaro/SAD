9bf0491516ed0cae3abe2c0d3ad155e8bd612013
dsapanaro <diana.sapanaro@gmail.com>
2014-11-11 20:30:13 -0500

    Artist name and region of Spain

--- SAD_Regions.py
+++ SAD_Regions.py
@@ -6,12 +6,9 @@ with open("colon3_table_export.csv", "r") as f:
 
 	reader = csv.reader(f)
 	for row in reader:
-		Spanish_Regions.append([row[1],row[20]])
+		Spanish_Regions.append([row[1], row[20]])
 			
 with open('Regions.csv', 'w') as csvfile:	
 	writer = csv.writer(csvfile)
 	for a_row in Spanish_Regions:
-    	writer.writerow(a_row)
-
-
-
+		writer.writerow(a_row)
