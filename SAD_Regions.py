935db0f361b13cb1b7b8c4f42ac172d3fdff44bc
dsapanaro <diana.sapanaro@gmail.com>
2014-11-13 13:05:43 -0500

    Spanish Regions

--- SAD_Regions.py
+++ SAD_Regions.py
@@ -8,7 +8,7 @@ with open("colon3_table_export.csv", "r") as f:
 	for row in reader:
 		Spanish_Regions.append([row[1], row[20]])
 			
-with open('Regions.csv', 'w') as csvfile:	
+with open('Regions.csv', 'w', encoding="ISO-8859-1") as csvfile:	
 	writer = csv.writer(csvfile)
 	for a_row in Spanish_Regions:
 		writer.writerow(a_row)
